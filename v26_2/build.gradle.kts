933fc4f68731e9302e59b1c016c4ca766e78a567
FilleDev <FilleDev@users.noreply.github.com>
2026-06-28 23:38:07 +0200

    Downgrade Adventure to 4.26.1 to prevent mismatch with Paper servers

--- v26_2/build.gradle.kts
+++ v26_2/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     compileOnly("org.spigotmc:spigot:26.2-R0.1-SNAPSHOT")
 
-    compileOnly("net.kyori:adventure-text-serializer-legacy:5.2.0")
+    compileOnly("net.kyori:adventure-text-serializer-legacy:4.26.1")
 
     implementation(project(":api"))
     implementation(project(":core"))
